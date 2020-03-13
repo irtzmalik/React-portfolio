634a40b28a0f9ac15a6472dd044ed329a22cbe8e
Irtaza Malik <irtz.malik@gmail.com>
2020-03-12 23:40:22 -0400

    adding files & folders

---
+++ src/components/projects.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+
+export default class Projects extends Component {
+  render() {
+    return (
+      <div>
+				<section className="colorlib-work" data-section="projects">
+					<div className="colorlib-narrow-content">
+						<div className="row">
+							<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+								<span className="heading-meta">My Work</span>
+								<h2 className="colorlib-heading animate-box">Recent Projects</h2>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+								<div className="project" style={{backgroundImage: 'url(images/img-1.png)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Work 01</a></h3>
+											<span>Password Generator</span>
+											<p className="icon">
+											<span><a href="#"><i className="icon-share3" /></a></span>	
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+								<div className="project" style={{backgroundImage: 'url(images/img-2.png)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Work 02</a></h3>
+											<span>Animation</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
+								<div className="project" style={{backgroundImage: 'url(images/img-3.png)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Work 03</a></h3>
+											<span>Illustration</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInBottom">
+								<div className="project" style={{backgroundImage: 'url(images/img-4.png)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Work 04</a></h3>
+											<span>Application</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+								<div className="project" style={{backgroundImage: 'url(images/img-5)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">TBA</a></h3>
+											<span>TBA,</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+
+											</p>
+										</div>
+									</div> Logo
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+								<div className="project" style={{backgroundImage: 'url(images/img-.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">TBA</a></h3>
+											<span>TBA</span>
+											<p className="icon">
+												<span><a href="#"><i className="icon-share3" /></a></span>
+
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-md-12 animate-box">
+								<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
+							</div>
+						</div>
+					</div>
+				</section>
+      </div>
+    )
+  }
+}
