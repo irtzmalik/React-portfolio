78761ffc885eda12d40b814050adf340e5b32849
Irtaza Malik <irtz.malik@gmail.com>
2020-03-14 20:57:53 -0400

    updated sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -17,7 +17,7 @@ export default class Sidebar extends Component {
                 <ul>
                   <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
                   <li><a href="#about" data-nav-section="about">About</a></li>
-                  <li><a href="#Projects" data-nav-section="Projects">Projects</a></li>
+                  <li><a href="#projects" data-nav-section="projects">Projects</a></li>
                 </ul>
               </div>
             </nav>
